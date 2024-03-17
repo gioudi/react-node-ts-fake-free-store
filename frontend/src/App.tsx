8dc0cd310a394812b150f6e8877fadb926ceccb8
gioudi <sergiopenagos881@gmail.com>
2024-03-16 20:53:37 -0500

    feat/front-end-structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Initial Demo
+      </header>
+    </div>
+  );
+}
+
+export default App;
