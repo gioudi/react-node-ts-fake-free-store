6962e2b8656d1dd5b4fe553575a863f7dfc8b636
gioudi <sergiopenagos881@gmail.com>
2024-03-17 00:03:49 -0500

    fix/setup-eslint-jest-stylelint

---
+++ frontend/jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
